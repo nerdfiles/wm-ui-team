13d0c36375812274b7714c697f41919c14d9e975
Aaron Alexander <nerdfiles@gmail.com>
2011-05-23 17:41:25 -0500

    clean

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'aalexan1');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'f0xf0x0!6');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
