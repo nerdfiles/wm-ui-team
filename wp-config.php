d630f46723f3b125d1bb0d2d85f3cef8131413f6
Aaron Alexander <aalexan1@wm.com>
2011-05-23 17:46:40 -0500

    cleanup

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'wp');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'f0xf0x0!6');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
